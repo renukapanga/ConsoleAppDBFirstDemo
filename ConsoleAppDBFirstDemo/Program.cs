c33714cde28129dec932f20cb58e32e2de5851a1
002CJE744 <002CJE744@LAPTOP-1T4DDRIK>
2021-09-22 15:45:23 +0530

    Add project files.

---
+++ ConsoleAppDBFirstDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppDBFirstDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new ibmEntities())
+            {
+                var query = from b in db.Persons                           
+                            orderby b.name
+                            select b;
+                Console.WriteLine("All Persons in the database:");
+                foreach (var item in query)
+                {
+                    Console.WriteLine(item.personid + "" + item.name + "" + item.city);
+                }
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey();
+
+            }
+        }
+    }
+}
